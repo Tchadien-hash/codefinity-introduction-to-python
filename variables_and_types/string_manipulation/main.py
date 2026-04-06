b438b190b22a189f263fe9291c7b03322469004c
Tchadien-hash <>
2026-04-06 23:35:05 +0000

    String Manipulation

---
+++ variables_and_types/string_manipulation/main.py
@@ -0,0 +1,10 @@
+grocery_items = "milk cheese bread apples oranges chicken"
+dairy1 = grocery_items[0:4]
+dairy2 = grocery_items[5:11]
+bakery1 = grocery_items[12:17]
+print(dairy1)
+print(dairy2)
+print(bakery1)
+aisle = 5
+message = f"We have dairy and bakery items: {dairy1}, {dairy2}, and {bakery1} in aisle {aisle}"
+print(message)
